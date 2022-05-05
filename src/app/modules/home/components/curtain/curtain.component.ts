9e1f662b1b460732c6740302759c5e7c2e69f393
Moises A. Guevara J <mguevaraj27@gmail.com>
2022-05-04 23:56:29 -0400

    Finished home styles
    - General styles.
    - Adaptation to mobile devices.

---
+++ src/app/modules/home/components/curtain/curtain.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curtain',
+  templateUrl: './curtain.component.html',
+  styleUrls: ['./curtain.component.scss']
+})
+export class CurtainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MaterialModule } from "../material/material.module";
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { CurtainComponent } from './components/curtain/curtain.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CurtainComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule,
-    MaterialModule
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
