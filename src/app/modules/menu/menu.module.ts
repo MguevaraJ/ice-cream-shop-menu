4b9e14515cd90b3dff823cff7f2f5f2fb396a3bf
Moises A. Guevara J <mguevaraj27@gmail.com>
2022-07-19 22:35:14 -0400

    ice-cream-item component completed

--- src/app/modules/menu/menu.module.ts
+++ src/app/modules/menu/menu.module.ts
@@ -7,12 +7,14 @@ import { MenuRoutingModule } from "./menu-routing.module";
 import { MenuComponent } from './components/menu/menu.component';
 import { MaterialModule } from "../material/material.module";
 import { BarComponent } from './components/bar/bar.component';
+import { IceCreamItemComponent } from './components/ice-cream-item/ice-cream-item.component';
 
 
 @NgModule({
   declarations: [
     MenuComponent,
-    BarComponent
+    BarComponent,
+    IceCreamItemComponent
   ],
   imports: [
     CommonModule,
